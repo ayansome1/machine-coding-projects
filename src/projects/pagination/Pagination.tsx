e621ca91a3c14a808f66ea3824c59f6acac6f37c
ayansome1 <ayansome1@gmail.com>
2024-04-28 14:58:57 +0530

    update

--- src/projects/pagination/Pagination.tsx
+++ src/projects/pagination/Pagination.tsx
@@ -1,15 +1,21 @@
 import styles from './Pagination.module.scss';
 import cx from 'classnames';
 
-const Pagination = ({ current, total, onChange }) => {
-  const getBtnClass = (index: number) => {
-    if (index === current - 1) {
+const Pagination = ({ current, total, onChange, perPage = 10 }) => {
+  let start = Math.max(0, current - Math.floor(perPage / 2));
+  const end = Math.min(start + perPage - 1, total - 1);
+  if (end - start + 1 < perPage) {
+    start = end - perPage + 1;
+  }
+
+  const getBtnClass = (pageIndex: number) => {
+    if (pageIndex === current) {
       return styles.active;
     }
     return '';
   };
 
-  const handleChange = (idx) => {
+  const handleChange = (idx: number) => {
     onChange(idx);
   };
 
@@ -21,23 +27,31 @@ const Pagination = ({ current, total, onChange }) => {
     onChange(current + 1);
   };
 
+  const getArray = () => {
+    const arr = [];
+    for (let i = start; i <= end; i++) {
+      arr.push(i);
+    }
+    return arr;
+  };
+
   return (
     <div>
-      <button onClick={handlePrev} disabled={current === 1}>
+      <button onClick={handlePrev} disabled={current === 0}>
         {'<'}
       </button>
-      {Array(total)
-        .fill(null)
-        .map((val, index) => (
-          <button
-            key={index}
-            className={cx(getBtnClass(index), styles.btn)}
-            onClick={() => handleChange(index + 1)}
-          >
-            {index + 1}
-          </button>
-        ))}
-      <button onClick={handleNext} disabled={current === total}>{'>'}</button>
+      {getArray().map((pageIndex) => (
+        <button
+          key={pageIndex}
+          className={cx(getBtnClass(pageIndex), styles.btn)}
+          onClick={() => handleChange(pageIndex)}
+        >
+          {pageIndex}
+        </button>
+      ))}
+      <button onClick={handleNext} disabled={current === total - 1}>
+        {'>'}
+      </button>
     </div>
   );
 };
--- src/projects/pagination/PaginationContainer.tsx
+++ src/projects/pagination/PaginationContainer.tsx
@@ -2,14 +2,14 @@ import { useState } from 'react';
 import Pagination from './Pagination';
 
 const PaginationContainer = () => {
-  const [current, setCurrent] = useState(1);
+  const [current, setCurrent] = useState(0);
 
   const onChange = (page) => {
     setCurrent(page);
   };
   return (
     <div>
-      <Pagination current={current} total={8} onChange={onChange} />
+      <Pagination current={current} total={20} onChange={onChange} perPage={4}/>
     </div>
   );
 };
